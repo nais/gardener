e4171bf7ebb388b6ea69a91e6cf37d3431d97ede
Audun Fauchald Strand <audunstrand@gmail.com>
2018-06-04 14:36:02 +0200

    refactored tests

--- CrashLoopBackoffFinder_test.go
+++ CrashLoopBackoffFinder_test.go
@@ -5,37 +5,55 @@ import (
 
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/fake"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestWillTriggerOn50Restarts(t *testing.T) {
 	k8sclient := fake.NewSimpleClientset()
-	pod := &v1.Pod{
-		Status: v1.PodStatus{ContainerStatuses: []v1.ContainerStatus{{RestartCount: 51}},
-		},
-	}
-	pod.Annotations = make(map[string]string)
+
+	namespace := "ns"
+	podname := "name"
+	pod := createPod(k8sclient, namespace, podname, 51)
+
 	triggered, _ := FindPodsInCrashloopBackoff(k8sclient, pod)
+
 	if triggered == false {
 		t.Fail()
 	}
-	if pod.GetAnnotations()["nais.io/gardener/status"] == "" || pod.GetAnnotations()["nais.io/gardener/status"] != "bad" {
+
+	pod1, _ := k8sclient.CoreV1().Pods(namespace).Get(podname, v12.GetOptions{})
+
+	if pod1.GetAnnotations()["nais.io/gardener/status"] == "" || pod1.GetAnnotations()["nais.io/gardener/status"] != "bad" {
 		t.Fail()
 	}
 }
 
 func TestWillNotTriggerOnLessThan50Restarts(t *testing.T) {
-	clientset := fake.NewSimpleClientset()
+	k8sclient := fake.NewSimpleClientset()
 
-	pod := &v1.Pod{
-		Status: v1.PodStatus{ContainerStatuses: []v1.ContainerStatus{{RestartCount: 49}},
-		},
-	}
-	pod.Annotations = make(map[string]string)
-	triggered, _ := FindPodsInCrashloopBackoff(clientset, pod)
+	namespace := "ns"
+	podname := "name"
+	pod := createPod(k8sclient, namespace, podname, 49)
+	triggered, _ := FindPodsInCrashloopBackoff(k8sclient, pod)
 	if triggered == true {
 		t.Fail()
 	}
-	if pod.GetAnnotations()["nais.io/gardener/status"] != "" || pod.GetAnnotations()["nais.io/gardener/status"] == "bad" {
+	pod1, _ := k8sclient.CoreV1().Pods(namespace).Get(podname, v12.GetOptions{})
+
+	if pod1.GetAnnotations()["nais.io/gardener/status"] != "" || pod1.GetAnnotations()["nais.io/gardener/status"] == "bad" {
 		t.Fail()
 	}
+}
+
+func createPod(k8sclient *fake.Clientset, namespace string, podName string, restarts int32) (*v1.Pod) {
+	pod := &v1.Pod{
+		Status: v1.PodStatus{ContainerStatuses: []v1.ContainerStatus{{RestartCount: restarts}},
+		},
+	}
+
+	pod.Name = podName
+	pod.Namespace = namespace
+	pod.Annotations = make(map[string]string)
+	k8sclient.CoreV1().Pods(namespace).Create(pod)
+	return pod
 }
