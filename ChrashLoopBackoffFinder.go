a3889293732bee9149fc6c533ddb55c282b98af9
Audun Fauchald Strand <audunstrand@gmail.com>
2018-06-08 11:45:29 +0200

    annotate deployment instead

--- ChrashLoopBackoffFinder.go
+++ ChrashLoopBackoffFinder.go
@@ -3,15 +3,27 @@ package main
 import (
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
+	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 )
 
 func FindPodsInCrashloopBackoff(client kubernetes.Interface, pod *v1.Pod) (bool, string) {
+
 	for _, containerStatus := range pod.Status.ContainerStatuses {
 		if containerStatus.RestartCount > 50 {
-			annotations := pod.GetAnnotations()
-			annotations["nais.io/gardener/status"] = "bad"
-			client.CoreV1().Pods(pod.Namespace).Update(pod)
-			return true, pod.Name
+			for _, set := range pod.OwnerReferences {
+				replicaSet, _ := client.AppsV1().ReplicaSets(pod.Namespace).Get(set.Name, v12.GetOptions{})
+				println("set: ", replicaSet.Name)
+
+				for _, depl := range replicaSet.OwnerReferences {
+					deployment, _ := client.AppsV1().Deployments(pod.Namespace).Get(depl.Name, v12.GetOptions{})
+					println("deployment: ", deployment.Name)
+					annotations := deployment.GetAnnotations()
+					annotations["nais.io/gardener/status"] = "bad"
+					client.AppsV1().Deployments(pod.Namespace).Update(deployment)
+					return true, pod.Name
+				}
+			}
 		}
 	}
 	return false, ""
--- CrashLoopBackoffFinder_test.go
+++ CrashLoopBackoffFinder_test.go
@@ -6,6 +6,7 @@ import (
 	"k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes/fake"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v13 "k8s.io/api/apps/v1"
 )
 
 func TestWillTriggerOn50Restarts(t *testing.T) {
@@ -21,9 +22,9 @@ func TestWillTriggerOn50Restarts(t *testing.T) {
 		t.Fail()
 	}
 
-	pod1, _ := k8sclient.CoreV1().Pods(namespace).Get(podname, v12.GetOptions{})
+	depl, _ := k8sclient.AppsV1().Deployments(namespace).Get("deployment", v12.GetOptions{})
 
-	if pod1.GetAnnotations()["nais.io/gardener/status"] == "" || pod1.GetAnnotations()["nais.io/gardener/status"] != "bad" {
+	if depl.GetAnnotations()["nais.io/gardener/status"] == "" || depl.GetAnnotations()["nais.io/gardener/status"] != "bad" {
 		t.Fail()
 	}
 }
@@ -38,22 +39,40 @@ func TestWillNotTriggerOnLessThan50Restarts(t *testing.T) {
 	if triggered == true {
 		t.Fail()
 	}
-	pod1, _ := k8sclient.CoreV1().Pods(namespace).Get(podname, v12.GetOptions{})
+	depl, _ := k8sclient.AppsV1().Deployments(namespace).Get("deployment", v12.GetOptions{})
 
-	if pod1.GetAnnotations()["nais.io/gardener/status"] != "" || pod1.GetAnnotations()["nais.io/gardener/status"] == "bad" {
+	if depl.GetAnnotations()["nais.io/gardener/status"] != "" || depl.GetAnnotations()["nais.io/gardener/status"] == "bad" {
 		t.Fail()
 	}
 }
 
 func createPod(k8sclient *fake.Clientset, namespace string, podName string, restarts int32) (*v1.Pod) {
+
+	deployment := &v13.Deployment{}
+	deployment.Name = "deployment"
+	deployment.Namespace = namespace
+	deployment.UID = "1234"
+	deployment.Annotations = make(map[string]string)
+
+
+	replicaSet := &v13.ReplicaSet{}
+	replicaSet.Name = "replicaSet"
+	replicaSet.Namespace = namespace
+	replicaSet.OwnerReferences = []v12.OwnerReference{{UID: deployment.GetUID()}}
+
 	pod := &v1.Pod{
 		Status: v1.PodStatus{ContainerStatuses: []v1.ContainerStatus{{RestartCount: restarts}},
 		},
 	}
+	pod.OwnerReferences = []v12.OwnerReference{{UID: replicaSet.GetUID()}}
 
 	pod.Name = podName
 	pod.Namespace = namespace
 	pod.Annotations = make(map[string]string)
+
+	k8sclient.AppsV1().Deployments(namespace).Create(deployment)
+	k8sclient.AppsV1().ReplicaSets(namespace).Create(replicaSet)
 	k8sclient.CoreV1().Pods(namespace).Create(pod)
+
 	return pod
 }
